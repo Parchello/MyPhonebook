8da9310d9713257e84f989bb15d8bf18230575a4
Parchello <parchellos@gmail.com>
2024-03-30 18:22:31 +0200

    name changed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook-2">
+        <BrowserRouter basename="/MyPhonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
